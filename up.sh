42b605ce134a474ba350b972f9423dbcfbcb72d4
Henrik Eriksson <henrik.eriksson@uadm.uu.se>
2014-12-18 10:43:37 +0100

    More info

--- up.sh
+++ up.sh
@@ -5,10 +5,18 @@ vagrant up
 mvn3 clean package -Dmaven.test.skip=true
 ( cd ../ladok3; mvn3 clean package -Dmaven.test.skip=true )
 
-echo "Note! use password 'vagrant' when using scp to vagrant vm:"
+# echo "Note! use password 'vagrant' when using scp to vagrant vm:"
+# scp -P 2222 \
+#    ../ladok3/ladok3atom-*/target/*.jar \
+#    */target/*.jar \
+#    vagrant@localhost:/opt/servicemix/apache-servicemix-5.1.2/deploy/
 
-scp -P 2222 \
-    ../ladok3/ladok3atom-*/target/*.jar \
+cp -v \
+	../ladok3/ladok3atom-*/target/*.jar \
     */target/*.jar \
-    vagrant@localhost:/opt/servicemix/apache-servicemix-5.1.2/deploy/
+    smx/deploy/
 
+echo "You might now be interested in doing:"
+echo "vagrant ssh"
+echo "tail -f /var/lib/servicemix/log/servicemix.log"
+echo "sudo service apache-servicemix stop|start"
