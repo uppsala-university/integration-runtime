057aa288b9da167955b5089c4bbbb1d64cc91df8
markus <markus@jardemalm.se>
2015-06-03 09:11:58 +0200

    Added type column.

--- provision/modules/mariadb/sql/init.sql
+++ provision/modules/mariadb/sql/init.sql
@@ -3,5 +3,6 @@ CREATE TABLE uuevents
    identifier varchar(255) PRIMARY KEY NOT NULL,
    issuedTime timestamp,
    producer varchar(255),
-   producerReferenceId varchar(255)
+   producerReferenceId varchar(255),
+   type varchar(255)
 );
