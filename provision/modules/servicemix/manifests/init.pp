e31ec7a716b9fd0fc139902d14673183003294c9
markus <markus@jardemalm.se>
2015-06-17 22:12:52 +0200

    Added a 2nd broker for dead letters (for use outside scope of JTA XA-transaction).

--- provision/modules/servicemix/manifests/init.pp
+++ provision/modules/servicemix/manifests/init.pp
@@ -4,19 +4,19 @@ class servicemix {
 
 	# Packages servicemix depends upon.
 	# But this is not declared in the servicemix rpm
-	# since there is no good "java" dependency.... openjdk, oracle jdk, ibm jdk etc.. 
+	# since there is no good "java" dependency.... openjdk, oracle jdk, ibm jdk etc..
 	package { "java-1.7.0-openjdk":
-    		ensure		=> present
-   	}
+  	ensure		=> present
+  }
 
-   	file { '/opt/rpm':
-   		ensure	=> directory
-   	}
+  file { '/opt/rpm':
+  	ensure		=> directory
+  }
 
-   	file { "/opt/rpm/apache-servicemix-$version-0.noarch.rpm":
-   		source 		=> "puppet:///modules/servicemix/apache-servicemix-$version-0.noarch.rpm",
-   		require		=> File['/opt/rpm']
-   	}
+  file { "/opt/rpm/apache-servicemix-$version-0.noarch.rpm":
+  	source 		=> "puppet:///modules/servicemix/apache-servicemix-$version-0.noarch.rpm",
+  	require		=> File['/opt/rpm']
+  }
 
 	package { 'apache-servicemix':
 		require 	=> File["/opt/rpm/apache-servicemix-$version-0.noarch.rpm"],
@@ -25,28 +25,51 @@ class servicemix {
 	}
 
 	file { "/opt/servicemix/apache-servicemix-$version/deploy/uu-features.xml":
-   		source 		=> 'puppet:///modules/servicemix/uu-features.xml',
-   		require		=> Service['apache-servicemix']
-   	}
-	
-	file { '/opt/servicemix/smx-relink.sh': 
+  	source 		=> 'puppet:///modules/servicemix/uu-features.xml',
+  	require		=> Service['apache-servicemix']
+  }
+
+	file { '/opt/servicemix/smx-relink.sh':
 		source 		=> 'puppet:///modules/servicemix/smx-relink.sh',
-		mode		=> 0755,
-		owner		=> root,
+		mode			=> 0755,
+		owner			=> root,
 		require		=> Package['apache-servicemix'],
+	}
+
+  file { '/opt/servicemix/.m2':
+  	ensure		=> directory,
+		owner			=> smx,
+		require		=> Package['apache-servicemix']
+  }
+
+#	file { "/opt/servicemix/apache-servicemix-$version/etc/org.apache.activemq.server-dlq.cfg":
+#		source 		=> 'puppet:///modules/servicemix/org.apache.activemq.server-dlq.cfg',
+#		require		=> Package['apache-servicemix']
+#	}
+
+	file { "/opt/servicemix/apache-servicemix-$version/etc/org.apache.activemq.server-default.cfg":
+		source 		=> 'puppet:///modules/servicemix/org.apache.activemq.server-default.cfg',
+		require		=> Package['apache-servicemix']
+	}
+
+	file { "/opt/servicemix/apache-servicemix-$version/etc/org.apache.activemq.server-dlq.cfg":
+		source 		=> 'puppet:///modules/servicemix/org.apache.activemq.server-dlq.cfg',
+		require		=> Package['apache-servicemix']
+	}
+
+	file { "/opt/servicemix/apache-servicemix-$version/etc/activemq.xml":
+		source 		=> 'puppet:///modules/servicemix/activemq.xml',
+		require		=> Package['apache-servicemix']
 	}
 
 	service { 'apache-servicemix':
 		enable		=> true,
 		ensure 		=> running,
 		require		=> [Package['java-1.7.0-openjdk'],
-				    Package['apache-servicemix']]
+									Package['apache-servicemix'],
+									File["/opt/servicemix/apache-servicemix-$version/etc/org.apache.activemq.server-default.cfg"],
+									File["/opt/servicemix/apache-servicemix-$version/etc/org.apache.activemq.server-dlq.cfg"],
+									File["/opt/servicemix/apache-servicemix-$version/etc/activemq.xml"]]
 	}
-
-   	file { '/opt/servicemix/.m2':
-   		ensure		=> directory,
-		owner		=> smx,
-		require		=> Package['apache-servicemix']
-   	}
 
 }
