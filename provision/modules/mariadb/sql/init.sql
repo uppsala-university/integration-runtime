3dddc427a8365870f4f2e4e220cded551ea18b2a
markus <markus@jardemalm.se>
2015-06-17 20:49:31 +0200

    Cleanup.

--- provision/modules/mariadb/sql/init.sql
+++ provision/modules/mariadb/sql/init.sql
@@ -1,8 +1,16 @@
 CREATE TABLE uuevents
 (
-   identifier varchar(255) PRIMARY KEY NOT NULL,
-   issuedTime timestamp,
-   producer varchar(255),
-   producerReferenceId varchar(255),
-   type varchar(255)
+  identifier varchar(255) PRIMARY KEY NOT NULL,
+  issuedTime timestamp,
+  producer varchar(255),
+  producerReferenceId varchar(255),
+  event_type varchar(255)
+);
+
+CREATE TABLE CAMEL_MESSAGEPROCESSED (
+  id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  processorName VARCHAR(255),
+  messageId VARCHAR(100),
+  createdAt TIMESTAMP,
+  UNIQUE KEY `ix_key` (`processorName`, `messageId`)
 );
