daf7fdacec2bcd4782bf06f9c3822734eadc73c3
markus <markus@jardemalm.se>
2015-11-09 14:32:37 +0100

    Added case sensitivity.

--- provision/modules/mariadb/sql/init.sql
+++ provision/modules/mariadb/sql/init.sql
@@ -2,15 +2,15 @@ CREATE TABLE uuevents
 (
   identifier varchar(255) PRIMARY KEY NOT NULL,
   issuedTime timestamp,
-  producer varchar(255),
-  producerReferenceId varchar(255),
-  event_type varchar(255)
+  producer varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  producerReferenceId varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  event_type varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
 );
 
 CREATE TABLE CAMEL_MESSAGEPROCESSED (
   id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  processorName VARCHAR(255),
-  messageId VARCHAR(100),
+  processorName VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  messageId VARCHAR(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   createdAt TIMESTAMP,
   UNIQUE KEY `ix_key` (`processorName`, `messageId`)
 );
