ac5e6b0a6abb5730f93c775a18c0cbffe49fc6e8
Henrik Eriksson <henrik.eriksson@uadm.uu.se>
2016-01-25 13:22:15 +0100

    Removing reduntant uu-features.xml

--- provision/modules/servicemix/manifests/init.pp
+++ provision/modules/servicemix/manifests/init.pp
@@ -24,13 +24,6 @@ class servicemix {
 		source 		=> "/opt/rpm/apache-servicemix-$version-0.noarch.rpm"
 	}
 
-	file { "/opt/servicemix/apache-servicemix-$version/deploy/uu-features.xml":
-  	source 		=> 'puppet:///modules/servicemix/uu-features.xml',
-  	require		=> Service['apache-servicemix'],
-		owner			=> smx,
-		group			=> smx
-  }
-
 	file { '/opt/servicemix/smx-relink.sh':
 		source 		=> 'puppet:///modules/servicemix/smx-relink.sh',
 		mode			=> 0755,
