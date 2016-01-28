4834fba3aae2206501da04aea5b5a7c6b31da66f
markus <markus@jardemalm.se>
2016-01-28 20:13:58 +0100

    Now uses karaf module instead of servicemix.

--- provision/manifests/default.pp
+++ provision/manifests/default.pp
@@ -1,11 +1,12 @@
 node "centos.dev.uu.se" {
 	#include devtools
 	include without-iptables
-	include servicemix
+	#include servicemix
+	include karaf
 	include ladok-dummy-webserver
 	include mariadb
 	#include neo4j
-	
+
 #	servicemix::smx-relink { 'deploy' :
 #		src		=> '/opt/servicemix/apache-servicemix-5.4.0/deploy',
 #		dest	=> '/vagrant/smx/deploy'
