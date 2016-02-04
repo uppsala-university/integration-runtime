96419a4617bf3e18468289abefdb56ab1b9a49c9
markus <markus@jardemalm.se>
2016-02-04 09:04:34 +0100

    Parameterized build script. Now totally resuable with other set of parameters.

--- activemq-rpm/build-rpm.sh
+++ activemq-rpm/build-rpm.sh
@@ -3,6 +3,9 @@
 PKG="apache-activemq"
 VERSION="5.13.0"
 TARBALL="$PKG-$VERSION-bin.tar.gz"
+ARCHITECTURE="x86_64"
+RESOURCE="http://apache.mirrors.spacedump.net/activemq"
+SPEC="$PKG.spec"
 
 echo "Building RPM for $PKG-$VERSION"
 
@@ -14,28 +17,28 @@ fi
 
 if [ ! -f $TARBALL ]
 then
-	curl http://apache.mirrors.spacedump.net/activemq/$VERSION/$TARBALL > $TARBALL
+	curl $RESOURCE/$VERSION/$TARBALL > $TARBALL
 
 fi
 
 echo -n "Patching specification..."
-sed -i.bak -e s/__PKG/${PKG}/g -e s/__VERSION/${VERSION}/g apache-activemq.spec
+sed -i.bak -e s/__PKG/${PKG}/g -e s/__VERSION/${VERSION}/g $SPEC
 echo " Done."
 
 yum -y install rpmdevtools && rpmdev-setuptree
-cp -v apache-activemq.spec ~/rpmbuild/SPECS/
+cp -v $SPEC ~/rpmbuild/SPECS/
 
 cp -v $TARBALL ~/rpmbuild/SOURCES/
-rpmbuild -bb ~/rpmbuild/SPECS/apache-activemq.spec
+rpmbuild -bb ~/rpmbuild/SPECS/$SPEC
 if [ $? -eq 0 ]
 then
-        cp -v /root/rpmbuild/RPMS/x86_64/apache-activemq-*.rpm .
+	cp -v /root/rpmbuild/RPMS/$ARCHITECTURE/$PKG-*.rpm .
 fi
 
 echo -n "Cleaning up..."
-if [ apache-activemq.spec.bak ]; then
-        rm apache-activemq.spec
-        mv apache-activemq.spec.bak apache-activemq.spec
+if [ $SPEC.bak ]; then
+        rm $SPEC
+        mv $SPEC.bak $SPEC
         echo " Done."
 else
         echo " Nothing to do."
