68e5904c3b151db1fa1b4ec9b63b9faa23fe66cb
markus <markus@jardemalm.se>
2016-02-05 10:21:30 +0100

    Changed Yum repo structure, just drop new RPM:s into directory file/yumrepo and it will be available.

--- environments/dev/modules/yumrepo/manifests/init.pp
+++ environments/dev/modules/yumrepo/manifests/init.pp
@@ -2,10 +2,8 @@ class yumrepo {
 
   exec { "Initialize local yum repository":
     command => "/usr/bin/createrepo /opt/yumrepo",
-    require => [Package["createrepo"],
-      File['/opt/yumrepo/apache-activemq-5.13.0-0.x86_64.rpm'],
-      File['/opt/yumrepo/apache-karaf-4.0.4-0.x86_64.rpm']]
-    }
+    require => [Package["createrepo"], File['/opt/yumrepo']]
+  }
 
   package { 'createrepo':
     ensure  => installed
@@ -15,18 +13,15 @@ class yumrepo {
     source  => "puppet:///modules/yumrepo/local.repo"
   }
 
-  file { '/opt/yumrepo':
-    ensure  => directory
-  }
-
-  file { "/opt/yumrepo/apache-activemq-5.13.0-0.x86_64.rpm":
-  	source   => "puppet:///modules/yumrepo/apache-activemq-5.13.0-0.x86_64.rpm",
-  	require  => File['/opt/yumrepo']
-  }
-
-  file { "/opt/yumrepo/apache-karaf-4.0.4-0.x86_64.rpm":
-  	source 		=> "puppet:///modules/yumrepo/apache-karaf-4.0.4-0.x86_64.rpm",
-  	require		=> File['/opt/yumrepo']
+  file { "/opt/yumrepo":
+    ensure    => directory,
+    recurse   => true,
+    purge     => true,
+    force     => true,
+    owner     => "root",
+    group     => "root",
+    mode      => "0644",
+    source    => "puppet:///modules/yumrepo/yumrepo"
   }
 
 }
