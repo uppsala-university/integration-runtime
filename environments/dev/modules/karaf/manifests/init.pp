357dd0600cfc79ea622e6e010eff36f97b5d81f3
markus <markus@jardemalm.se>
2016-02-10 09:07:11 +0100

    Fixed quoting marks for variable evaluation.

--- environments/dev/modules/karaf/manifests/init.pp
+++ environments/dev/modules/karaf/manifests/init.pp
@@ -1,25 +1,27 @@
 class karaf {
 
-	$version = "4.0.4"
+	$major_minor = "3.0"
+	$patch = "5"
+	$version = "${major_minor}.${patch}"
 
 	package { "apache-karaf-$version-0.x86_64":
-    ensure    => installed,
-    require   => Class['yumrepo']
+    		ensure    => installed,
+    		require   => Class['yumrepo']
   }
 
 	file { "/opt/apache-karaf/etc/org.ops4j.pax.url.mvn.cfg":
-		source 		=> 'puppet:///modules/karaf/org.ops4j.pax.url.mvn.cfg',
-		mode			=> "0644",
-		owner			=> karaf,
-		group			=> karaf,
+		source 		=> "puppet:///modules/karaf/$major_minor.x/org.ops4j.pax.url.mvn.cfg",
+		mode		=> "0644",
+		owner		=> karaf,
+		group		=> karaf,
 		require		=> Package["apache-karaf-$version-0.x86_64"]
 	}
 
 	file { "/opt/apache-karaf/deploy/integration-plattform.xml":
-		source 		=> 'puppet:///modules/karaf/integration-plattform.xml',
-		mode			=> "0644",
-		owner			=> karaf,
-		group			=> karaf,
+		source 		=> "puppet:///modules/karaf/$major_minor.x/integration-plattform.xml",
+		mode		=> "0644",
+		owner		=> karaf,
+		group		=> karaf,
 		require		=> Package["apache-karaf-$version-0.x86_64"]
 	}
 
@@ -27,7 +29,7 @@ class karaf {
 		enable		=> true,
 		ensure 		=> running,
 		require		=> [Package["apache-karaf-$version-0.x86_64"],
-									File['/opt/apache-karaf/etc/org.ops4j.pax.url.mvn.cfg']]
+					File['/opt/apache-karaf/etc/org.ops4j.pax.url.mvn.cfg']]
 	}
 
 }
